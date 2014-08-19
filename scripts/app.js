47ad169b39f0ac7546067d1361dc9c069a3dc8bd
jerkovicl <luka.jerkovic@sistemi.hr>
2014-08-19 15:11:02 +0200

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -6,13 +6,13 @@ var configFunction = function ($routeProvider,$locationProvider) {
     'use strict';
     $routeProvider
         .when('/hypebeast', {
-            templateUrl: 'views/hypebeast.html'
+            templateUrl: '/views/hypebeast.html'
         })
         .when('/victorcruz', {
-            templateUrl: 'views/teamvic.html'
+            templateUrl: '/views/teamvic.html'
         })
         .when('/highsnobiety', {
-            templateUrl: 'views/highsnobiety.html'
+            templateUrl: '/views/highsnobiety.html'
         })
         .otherwise({
             redirectTo: '/'
